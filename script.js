c00165c845462fb1a73b6e9ded70c1ea2878a898
Ritwik-main <10britwikbis.aggarwal@gmail.com>
2024-07-28 01:05:37 +0530

    Revert "youneedpizza"
    This reverts commit aa0cb2af9c882b521e5b7a238920e4a1971cf551.

--- script.js
+++ script.js
@@ -1,216 +1,218 @@
-$(document).ready(function() {
-    var particles = [];
-    var maxParticles = 20;
-
-    function createParticle(e) {
-        var x = e.pageX - $(this).offset().left;
-        var y = e.pageY - $(this).offset().top;
-        var size = Math.random() * 5 + 1;
-        var color = 'hsl(' + Math.random() * 360 + ', 80%, 50%)';
-
-        var particle = $('<div class="particle"></div>');
-        particle.css({
-            left: x,
-            top: y,
-            width: size + 'px',
-            height: size + 'px',
-            backgroundColor: color
-        });
-
-        $('#particle-container').append(particle);
-        particles.push(particle);
-
-        if (particles.length > maxParticles) {
-            particles.shift().remove();
+$(document).ready(function () {
+  // typing animation
+  (function ($) {
+    $.fn.writeText = function (content) {
+      var contentArray = content.split(""),
+        current = 0,
+        elem = this;
+      setInterval(function () {
+        if (current < contentArray.length) {
+          elem.text(elem.text() + contentArray[current++]);
         }
-    }
-
-    $('#particle-container').on('mousemove', function(e) {
-        createParticle.call(this, e);
-    });
-
-    // Pizza trail effect with controlled frequency
-    var lastPizzaTime = 0;
-    var pizzaInterval = 500; // Interval in milliseconds
-
-    $(document).on('mousemove', function(event) {
-        var currentTime = new Date().getTime();
-        if (currentTime - lastPizzaTime > pizzaInterval) {
-            const trailContainer = $('#trail-container');
-            const trail = $('<div class="trail">🍕</div>');
-            trail.css({
-                top: `${event.clientY}px`,
-                left: `${event.clientX}px`
-            });
-
-            trailContainer.append(trail);
-
-            setTimeout(() => {
-                trail.remove();
-            }, 1000); // 1 second to match the CSS animation duration
-
-            lastPizzaTime = currentTime;
-        }
-    });
-
-    // typing animation
-    (function ($) {
-        $.fn.writeText = function (content) {
-            var contentArray = content.split(""),
-                current = 0,
-                elem = this;
-            setInterval(function () {
-                if (current < contentArray.length) {
-                    elem.text(elem.text() + contentArray[current++]);
-                }
-            }, 80);
-        };
-    })(jQuery);
-
-    // input text for typing animation
-    $("#holder").writeText("TECH ENTHUSIAST + STUDENT (pizza is life)");
-
-    // initialize wow.js
-    new WOW().init();
-
-    // Push the body and the nav over by 285px over
-    var main = function () {
-        $(".fa-bars").click(function () {
-            $(".nav-screen").animate(
-                {
-                    right: "0px"
-                },
-                200
-            );
-
-            $("body").animate(
-                {
-                    right: "285px"
-                },
-                200
-            );
-        });
-
-        // Then push them back
-        $(".fa-times").click(function () {
-            $(".nav-screen").animate(
-                {
-                    right: "-285px"
-                },
-                200
-            );
-
-            $("body").animate(
-                {
-                    right: "0px"
-                },
-                200
-            );
-        });
-
-        $(".nav-links a").click(function () {
-            $(".nav-screen").animate(
-                {
-                    right: "-285px"
-                },
-                500
-            );
-
-            $("body").animate(
-                {
-                    right: "0px"
-                },
-                500
-            );
-        });
+      }, 80);
     };
+  })(jQuery);
 
-    $(document).ready(main);
+  // input text for typing animation
+  $("#holder").writeText("TECH ENTHUSIAST + STUDENT");
 
-    // initiate full page scroll
-    $("#fullpage").fullpage({
-        scrollBar: true,
-        responsiveWidth: 400,
-        navigation: true,
-        navigationTooltips: ["home", "about", "portfolio", "contact", "connect"],
-        anchors: ["home", "about", "portfolio", "contact", "connect"],
-        menu: "#myMenu",
-        fitToSection: false,
+  // initialize wow.js
+  new WOW().init();
 
-        afterLoad: function (anchorLink, index) {
-            var loadedSection = $(this);
+  // Push the body and the nav over by 285px over
+  var main = function () {
+    $(".fa-bars").click(function () {
+      $(".nav-screen").animate(
+        {
+          right: "0px"
+        },
+        200
+      );
 
-            //using index
-            if (index == 1) {
-                $(".fa-chevron-down").each(function () {
-                    $(this).css("opacity", "1");
-                });
-                $(".header-links a").each(function () {
-                    $(this).css("color", "white");
-                });
-                $(".header-links").css("background-color", "transparent");
-            } else if (index != 1) {
-                $(".header-links a").each(function () {
-                    $(this).css("color", "black");
-                });
-                $(".header-links").css("background-color", "white");
-            }
+      $("body").animate(
+        {
+          right: "285px"
+        },
+        200
+      );
+    });
 
-            if (index == 2) {
-                $(".skillbar").each(function () {
-                    $(this)
-                        .find(".skillbar-bar")
-                        .animate(
-                            {
-                                width: $(this).attr("data-percent")
-                            },
-                            2500
-                        );
-                });
-            }
+    // Then push them back */
+    $(".fa-times").click(function () {
+      $(".nav-screen").animate(
+        {
+          right: "-285px"
+        },
+        200
+      );
+
+      $("body").animate(
+        {
+          right: "0px"
+        },
+        200
+      );
+    });
+
+    $(".nav-links a").click(function () {
+      $(".nav-screen").animate(
+        {
+          right: "-285px"
+        },
+        500
+      );
+
+      $("body").animate(
+        {
+          right: "0px"
+        },
+        500
+      );
+    });
+  };
+
+  $(document).ready(main);
+
+  // initiate full page scroll
+
+  $("#fullpage").fullpage({
+    scrollBar: true,
+    responsiveWidth: 400,
+    navigation: true,
+    navigationTooltips: ["home", "about", "portfolio", "contact", "connect"],
+    anchors: ["home", "about", "portfolio", "contact", "connect"],
+    menu: "#myMenu",
+    fitToSection: false,
+
+    afterLoad: function (anchorLink, index) {
+      var loadedSection = $(this);
+
+      //using index
+      if (index == 1) {
+        /* add opacity to arrow */
+        $(".fa-chevron-down").each(function () {
+          $(this).css("opacity", "1");
+        });
+        $(".header-links a").each(function () {
+          $(this).css("color", "white");
+        });
+        $(".header-links").css("background-color", "transparent");
+      } else if (index != 1) {
+        $(".header-links a").each(function () {
+          $(this).css("color", "black");
+        });
+        $(".header-links").css("background-color", "white");
+      }
+
+      //using index
+      if (index == 2) {
+        /* animate skill bars */
+        $(".skillbar").each(function () {
+          $(this)
+            .find(".skillbar-bar")
+            .animate(
+              {
+                width: $(this).attr("data-percent")
+              },
+              2500
+            );
+        });
+      }
+    }
+  });
+
+  // move section down one
+  $(document).on("click", "#moveDown", function () {
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  // fullpage.js link navigation
+  $(document).on("click", "#skills", function () {
+    $.fn.fullpage.moveTo(2);
+  });
+
+  $(document).on("click", "#projects", function () {
+    $.fn.fullpage.moveTo(3);
+  });
+
+  $(document).on("click", "#contact", function () {
+    $.fn.fullpage.moveTo(4);
+  });
+
+  // smooth scrolling
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            700
+          );
+          return false;
         }
+      }
     });
+  });
 
-    // move section down one
-    $(document).on("click", "#moveDown", function () {
-        $.fn.fullpage.moveSectionDown();
-    });
+  //ajax form
+  $(function () {
+    // Get the form.
+    var form = $("#ajax-contact");
 
-    // fullpage.js link navigation
-    $(document).on("click", "#skills", function () {
-        $.fn.fullpage.moveTo(2);
-    });
+    // Get the messages div.
+    var formMessages = $("#form-messages");
 
-    $(document).on("click", "#projects", function () {
-        $.fn.fullpage.moveTo(3);
-    });
+    // Set up an event listener for the contact form.
+    $(form).submit(function (e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
 
-    $(document).on("click", "#contact", function () {
-        $.fn.fullpage.moveTo(4);
-    });
+      // Serialize the form data.
+      var formData = $(form).serialize();
 
-    // smooth scrolling
-    $(function () {
-        $("a[href*=#]:not([href=#])").click(function () {
-            if (
-                location.pathname.replace(/^\//, "") ==
-                    this.pathname.replace(/^\//, "") &&
-                location.hostname == this.hostname
-            ) {
-                var target = $(this.hash);
-                target = target.length
-                    ? target
-                    : $("[name=" + this.hash.slice(1) + "]");
-                if (target.length) {
-                    $("html,body").animate(
-                        {
-                            scrollTop: target.offset().top
-                        },
-                        700
-                    );
-                    return false;
-                }
-            }
+      // Submit the form using AJAX.
+      $.ajax({
+        type: "POST",
+        url: $(form).attr("action"),
+        data: formData
+      })
+        .done(function (response) {
+          // Make sure that the formMessages div has the 'success' class.
+          $(formMessages).removeClass("error");
+          $(formMessages).addClass("success");
+
+          // Set the message text.
+          $(formMessages).text(response);
+
+          // Clear the form.
+          $("#name").val("");
+          $("#email").val("");
+          $("#message").val("");
+        })
+        .fail(function (data) {
+          // Make sure that the formMessages div has the 'error' class.
+          $(formMessages).removeClass("success");
+          $(formMessages).addClass("error");
+
+          // Set the message text.
+          if (data.responseText !== "") {
+            $(formMessages).text(data.responseText);
+          } else {
+            $(formMessages).text(
+              "Oops! An error occured and your message could not be sent."
+            );
+          }
         });
     });
+  });
 });
